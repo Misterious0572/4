e83ec4427acbdc005f6c8da3b9a6dbb5c36cfccf
Demo <prathamesh@mail.com>
2023-10-31 12:24:58 +0530

    Initial commit

---
+++ Addition.java
@@ -0,0 +1,15 @@
+public class Addition
+{
+    public static int addition
+    {
+        return number1+number2 ;
+        return number1-number2 ;
+        return number1*number2 ;
+        return number1/number2 ;
+        return number1%number2 ;
+
+        return "I am attending DevOps Lab.."
+        return "I am a TEIT student"
+        return "4/10/2023"
+    }
+}
